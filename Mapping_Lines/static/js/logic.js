46a53104539ed8995401e68c22dae5da30c7b1c8
Angela Dominguez <aodoming154@gmail.com>
2020-01-07 23:19:15 -0800

    Adding Files for Mapping_Multiple_Points

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,100 +1,63 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
+//                   illustrating how lines are mapped by first mapping the airline route from
+//               Los Angeles to San Francisco. Mapping airline routes will help us understand how tectonic plate data is added to a map.
 
+
+// Add console.log to check to see if our code is working.
+	console.log("working");
+
+// Map the airline route from Los Angeles to San Francisco.
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 14);
+	let map = L.map('mapid').setView([[36.1733, -120.1794]], 7);
+
+// Coordinates for each point to be used in the line.
+// Coordinates need to be in an array. We can assign the array to the line variable like this:
+	let line = [
+		[33.9416, -118.4085],
+		[37.6213, -122.3790]
+	];
+
+// Create a line on a map using the Leaflet polyline() function.
+// Create a polyline using the line coordinates and make the line red.
+	L.polyline(line, {
+		color: "red"
+	}).addTo(map);
 
 
 // In the logic.js file, replace the marker variable (which we used to map one location) with the
 // cities variable that references the five most populous cities array in the following code block.
 // An array containing each city's location, state, and population.
-// let cities = [{
-// 	location: [40.7128, -74.0059],
-// 	city: "New York City",
-// 	state: "NY",
-// 	population: 8398748
-//   },
-//   {
-// 	location: [41.8781, -87.6298],
-// 	city: "Chicago",
-// 	state: "IL",
-// 	population: 2705994
-//   },
-//   {
-// 	location: [29.7604, -95.3698],
-// 	city: "Houston",
-// 	state: "TX",
-// 	population: 2325502
-//   },
-//   {
-// 	location: [34.0522, -118.2437],
-// 	city: "Los Angeles",
-// 	state: "CA",
-// 	population: 3990456
-//   },
-//   {
-// 	location: [33.4484, -112.0740],
-// 	city: "Phoenix",
-// 	state: "AZ",
-// 	population: 1660272
-//   }
-//   ];
+	let cities = [{
+		location: [40.7128, -74.0059],
+		city: "New York City",
+		state: "NY",
+		population: 8398748
+	},
+	{
+		location: [41.8781, -87.6298],
+		city: "Chicago",
+		state: "IL",
+		population: 2705994
+	},
+	{
+		location: [29.7604, -95.3698],
+		city: "Houston",
+		state: "TX",
+		population: 2325502
+	},
+	{
+		location: [34.0522, -118.2437],
+		city: "Los Angeles",
+		state: "CA",
+		population: 3990456
+	},
+	{
+		location: [33.4484, -112.0740],
+		city: "Phoenix",
+		state: "AZ",
+		population: 1660272
+	}
+	];
 
-// Add a variable and assign it to the cities array. Get data from cities.js
-let cityData = cities;
-
-// Loop (iterate) through the cities array and create one marker for each city.
-// Use the console.log() function inside the {} to print each object in the array to the console:
-	// cityData.forEach(function(city) {
-	// 	console.log(city)
-	// });
-
-// Add each city’s location to the map by adding the location to the marker() function.
-	// cityData.forEach(function(city) {
-	// 	console.log(city)
-	// 	L.marker(city.location).addTo(map);
-	// });
-
-
-// 									Bind a Popup to the Marker.
-// Use Leaflet’s bindPopup() method on the marker() function to create one marker for each city.
-// Edit the forEach function (above code) and add the bindPopup() method. Inside the parentheses of the bindPopup()
-// retrieve the name of the city, state, and population.
-// Also,format the population with a thousands separator by using the toLocaleString() method on
-//  the city.population in the bindPopup() method
-
-	// cityData.forEach(function(city) {
-	// 	console.log(city)
-	// 	L.marker(city.location)
-	// 	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString()+ "</h3>")
-	// 	.addTo(map);
-	// });
-
-// Next, change the marker for each city to a circle that has a radius equivalent 
-// to the city’s population. Use the circleMarker() function. Assign the “radius” key to the population by using city.population.
-
-cityData.forEach(function(city) {
-	console.log(city)
-	L.circleMarker(city.location, {
-		radius: city.population/100000
-	})
-	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString()+ "</h3>")
-  	.addTo(map);
-});
-
-// Skill Drill
-// Edit the logic.js file to create an orange circle popup marker for each city, 
-// with a lineweight of 4, a radius where the population number is decreased by 200,000, 
-//  that's on a dark map
-
-// cityData.forEach(function(city) {
-// 	console.log(city)
-// 	L.circleMarker(city.location, {
-// 		radius: city.population/200000,
-// 		color: "orange"
-// 	})
-// 	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString()+ "</h3>")
-//   	.addTo(map);
 
 // Create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
