83bc7579c6b61c009328b1f2ab6f8507c1728410
Angela Dominguez <aodoming154@gmail.com>
2020-01-08 00:26:22 -0800

    Adding Files that Map Lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -22,41 +22,22 @@
 		color: "red"
 	}).addTo(map);
 
+// Get data from cities.js
+//  It's a best practice to have the data in an external file and refer to that file
+let cityData = cities;
 
-// In the logic.js file, replace the marker variable (which we used to map one location) with the
-// cities variable that references the five most populous cities array in the following code block.
-// An array containing each city's location, state, and population.
-	let cities = [{
-		location: [40.7128, -74.0059],
-		city: "New York City",
-		state: "NY",
-		population: 8398748
-	},
-	{
-		location: [41.8781, -87.6298],
-		city: "Chicago",
-		state: "IL",
-		population: 2705994
-	},
-	{
-		location: [29.7604, -95.3698],
-		city: "Houston",
-		state: "TX",
-		population: 2325502
-	},
-	{
-		location: [34.0522, -118.2437],
-		city: "Los Angeles",
-		state: "CA",
-		population: 3990456
-	},
-	{
-		location: [33.4484, -112.0740],
-		city: "Phoenix",
-		state: "AZ",
-		population: 1660272
-	}
-	];
+// change the marker for each city to a circle that has a radius equivalent to the city’s population.
+// Assign the “radius” key to the population by using city.population.
+// Decrease each city's radius so the circle markers fit on the map; by divide the city.population value by “100000”
+
+cityData.forEach(function(city) {
+	console.log(city)
+L.circleMarker(city.location, {
+	radius:city.population/100000
+})
+.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+.addTo(map);
+});
 
 
 // Create the tile layer that will be the background of our map.
