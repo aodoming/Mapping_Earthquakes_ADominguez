c7bd376eb624cda1984c1ffc3d18424fab451d0f
Angela Dominguez <aodoming154@gmail.com>
2020-01-09 22:57:46 -0800

    Adding Files for tracking Earthquakes_past7days

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -1,6 +1,9 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+// Create the map object with center of the Earth
+	//let map = L.map('mapid').setView([30, 30], 2);
+
 // Map a GeoJSON Point. Add GeoJSON data.
 	let sanFranAirport =
 	{"type":"FeatureCollection","features":[{
@@ -65,9 +68,42 @@ console.log("working");
 
 // Retrieve the earthquake GeoJSON data.
 	d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-	// Creating a GeoJSON layer with the retrieved data.
-	L.geoJson(data).addTo(map);
-	});
 
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+    function styleInfo(feature) {
+        return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: "#ffae42",
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+        };
+    }
+
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+        return 1;
+        }
+        return magnitude * 4;
+    }
+
+// Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+
+        // We turn each feature into a circleMarker on the map.
+        pointToLayer: function(feature, latlng) {
+                    console.log(data);
+                    return L.circleMarker(latlng);
+                },
+            style: styleInfo
+            }).addTo(map);
+        });
+        
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
